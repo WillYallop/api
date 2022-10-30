6b84902eb0e6af01289c439decc22fc0fc763045
WillYallop <hello@williamyallop.com>
2022-10-30 16:23:57 +0000

    Removed types directory temp

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,21 @@ declare global {
       };
     }
   }
+
+  interface NIVErrorRes {
+    [key: string]: Array<{
+      rule: string;
+      message: string;
+    }>;
+  }
+  type RecaptchaErrors = Array<string>;
+
+  interface ErrorResponse {
+    code: number;
+    message: string;
+    type: "VALIDATION" | "RECAPTCHA" | "UNKNOWN" | "EMAIL";
+    errors?: NIVErrorRes | RecaptchaErrors;
+  }
 }
 
 // Middleware
